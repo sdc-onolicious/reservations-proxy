7a76a57b2838d8b32eb8bc99dc7b31d0d1b5fb61
Sam Hodak <slhodak@gmail.com>
2019-05-29 16:52:54 -0700

    Rename and order target divs; allow for restaurant id parameter in url

--- server/index.js
+++ server/index.js
@@ -4,4 +4,15 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../public')));
 
-app.listen(3000, () => {console.log('Open Table proxy server listening on port 3000!')});
+app.get('/:id', (req, res) => {
+  if (!req.params.id) {
+    res.status(400);
+    res.end();
+  } else {
+    res.sendFile('index.html', { root: path.resolve(__dirname, '../public') });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Open Table proxy server listening on port 3000!');
+});
