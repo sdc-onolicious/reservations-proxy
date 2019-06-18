e917aacfdbc78a3bad0b1a6e2c7550e34735ad57
William Chan <wchan1992@gmail.com>
2019-06-18 11:54:23 -0700

    Update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import http from "k6/http";
 import { check, sleep } from "k6";
 export let options = {
-  vus: 100,
+  vus: 250,
   duration: "3m",
   // rps: 200,
 };
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const axios = require('axios');
+const request = require('request');
 
 app.use('/', express.static(path.join(__dirname, '../public')));
 
@@ -19,18 +20,26 @@ app.get('/restaurants/:id/reservations', (req, res) => {
   .catch(error => {
     res.status(500).send();
   });
+  // request
+  // .get(`http://localhost:3020/restaurants/${id}/reservations`)
+  // .on('data', (data) => {
+  //   res.send(data).status(200);
+  // })
+  // .on('error', (err) => {
+  //   res.sendStatus(500);
+  // })
 });
 
-app.post('/restaurants/:id/reservations', (req, res) => {
-  const { id } = req.params;
-  axios.get(`http://localhost:3020/restaurants/${id}/reservations`)
-  .then(response => {
-    res.status(201).send(response.data);
-  })
-  .catch(error => {
-    res.status(500).send();
-  });
-});
+// app.post('/restaurants/:id/reservations', (req, res) => {
+//   const { id } = req.params;
+//   axios.get(`http://localhost:3020/restaurants/${id}/reservations`)
+//   .then(response => {
+//     res.status(201).send(response.data);
+//   })
+//   .catch(error => {
+//     res.status(500).send();
+//   });
+// });
 
 app.listen(3000, () => {
   console.log('Open Table proxy server listening on port 3000!');
